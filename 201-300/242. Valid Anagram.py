fe37735ac82e897012df767a5bf20e1d0c34985f
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-03-07 19:17:42 +0800

    Create 242. Valid Anagram.py

---
+++ 201-300/242. Valid Anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        return collections.Counter(s)==collections.Counter(t)
+        
