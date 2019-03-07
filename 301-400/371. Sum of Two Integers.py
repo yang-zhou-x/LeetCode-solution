425c23283e07e9f1ef83a1ee3df19514f4c1d5c4
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-03-07 17:56:42 +0800

    Create 371. Sum of Two Integers.py

---
+++ 301-400/371. Sum of Two Integers.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return sum((a,b))
+
+# 题意可能是希望使用按位操作符吧
