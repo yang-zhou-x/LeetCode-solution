b49d842454efe1a4c3ff8f0719cccf22ba0c9b08
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-03-10 20:08:16 +0800

    Create 28. Implement strStr().py

---
+++ 1-100/28. Implement strStr().py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
+        
+# Runtime: 16 ms, faster than 100.00% of Python online submissions for Implement strStr().
+# Memory Usage: 11 MB, less than 30.73% of Python online submissions for Implement strStr().
