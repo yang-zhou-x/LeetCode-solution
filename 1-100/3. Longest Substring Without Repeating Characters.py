545e8b4c6b3c5568fb02ac88bd31b27fd0f8b4da
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-03-31 11:41:50 +0800

    Update 3. Longest Substring Without Repeating Characters.py

--- 1-100/3. Longest Substring Without Repeating Characters.py
+++ 1-100/3. Longest Substring Without Repeating Characters.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        start=0  # 子串的起始位置
+        start=0 # Substring的起始位置
         ans=0
         cache={}  # 用来记录每个字母最近一次出现的位置
         for i,ss in enumerate(s):
             if ss in cache and start<=cache[ss]:
                 start=cache[ss]+1
-                cache[ss]=i
+                cache[ss]=i # 更新位置
             else:
                 ans=max(ans,i-start+1)
-                cache[ss]=i
+                cache[ss]=i # 添加位置
         return ans
-    
-# Runtime: 44 ms, faster than 99.47% of Python online submissions for Longest Substring Without Repeating Characters.
+        
+# Runtime: 44 ms, faster than 99.14% of Python online submissions for Longest Substring Without Repeating Characters.
 # Memory Usage: 11.2 MB, less than 49.60% of Python online submissions for Longest Substring Without Repeating Characters.
