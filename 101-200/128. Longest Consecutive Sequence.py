c115c3f6ba25a90322233735ac8d94e080715581
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-04-06 13:43:35 +0800

    Update 128. Longest Consecutive Sequence.py

--- 101-200/128. Longest Consecutive Sequence.py
+++ 101-200/128. Longest Consecutive Sequence.py
@@ -5,12 +5,12 @@ class Solution:
         for n in nums:
             # nums可以看作由若干个连续片段组成(最短为1)，只考虑每段的开头
             if n-1 not in nums: # 说明n是序列起点
-                curr=n
                 length=1
-                while curr+1 in nums: # 遍历该序列
-                    curr+=1
+                while n+1 in nums: # 遍历该序列
+                    n+=1
                     length+=1
                 longest=max(longest,length)
         return longest
-# Runtime: 40 ms, faster than 89.78% of Python3 online submissions for Longest Consecutive Sequence.
-# Memory Usage: 14.3 MB, less than 5.31% of Python3 online submissions for Longest Consecutive Sequence.
+    
+# Runtime: 40 ms, faster than 89.86% of Python3 online submissions for Longest Consecutive Sequence.
+# Memory Usage: 14.2 MB, less than 5.31% of Python3 online submissions for Longest Consecutive Sequence.
