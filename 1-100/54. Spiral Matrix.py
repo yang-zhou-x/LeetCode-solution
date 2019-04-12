67bf97b6ef752219091d1b11610ed07098fcaaff
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-04-12 23:52:22 +0800

    Update 54. Spiral Matrix.py

--- 1-100/54. Spiral Matrix.py
+++ 1-100/54. Spiral Matrix.py
@@ -29,3 +29,33 @@ class Solution:
 # Memory Usage: 13 MB, less than 5.18% of Python3 online submissions for Spiral Matrix.
 
 # 解法2: 思想与解法1相同，但是分为上右下左四段，非逐个
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return []
+        r_s,r_e=0,len(matrix)-1 # 索引
+        c_s,c_e=0,len(matrix[0])-1 # 索引
+        ans=[]
+        flag='head'
+        while r_s<=r_e and c_s<=c_e:
+            if flag=='head':
+                ans+=matrix[r_s][c_s:c_e+1]
+                r_s+=1
+                flag='right'
+            elif flag=='right':
+                ans+=[matrix[r][c_e] for r in range(r_s,r_e+1)]
+                c_e-=1
+                flag='tail'
+            elif flag=='tail':
+                sub=matrix[r_e][c_s:c_e+1]
+                sub.reverse()
+                ans+=sub
+                r_e-=1
+                flag='left'
+            elif flag=='left':
+                ans+=[matrix[r][c_s] for r in range(r_e,r_s-1,-1)]
+                c_s+=1
+                flag='head'
+        return ans
+# Runtime: 36 ms, faster than 73.14% of Python3 online submissions for Spiral Matrix.
+# Memory Usage: 13.1 MB, less than 5.18% of Python3 online submissions for Spiral Matrix.
