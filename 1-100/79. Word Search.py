3ff031355875fa226c24aa6966be4afa7d83e0d4
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-04-20 23:08:07 +0800

    Update 79. Word Search.py

--- 1-100/79. Word Search.py
+++ 1-100/79. Word Search.py
@@ -25,4 +25,38 @@ class Solution:
 
 # 改进方法
 # 1）先计数，从字符数量上判断是否存在可能
-# 2）去掉list的slice操作
+# 2）去掉list的slice操作，改用索引
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if not board or not board[0] or not word:
+            return False
+        # 字符计数
+        count = collections.Counter(c for row in board for c in row)
+        # 从字符类别和数量上先判断一下
+        for c, cnt in collections.Counter(word).items():
+            if c not in count or cnt > count[c]:
+                return False
+        
+        def dfs(i, x, y): # 从board[x][y]开始，寻找周围是否有word[i]
+            if i >= len(word): # 终止
+                return True
+            
+            c = board[x][y]
+            board[x][y] = '' # 不能重复使用，先用空字符占位
+            
+            if x > 0 and board[x - 1][y] == word[i] and dfs(i + 1, x - 1, y):
+                return True
+            if x + 1 < m and board[x + 1][y] == word[i] and dfs(i + 1, x + 1, y):
+                return True
+            if y > 0 and board[x][y - 1] == word[i] and dfs(i + 1, x, y - 1):
+                return True
+            if y + 1 < n and board[x][y + 1] == word[i] and dfs(i + 1, x, y + 1):
+                return True
+            
+            board[x][y] = c
+            return False
+        
+        m, n = len(board), len(board[0])
+        return any(dfs(1, x, y) for x in range(m) for y in range(n) if board[x][y] == word[0])  # 只要存在1个True，返回True
+# Runtime: 56 ms, faster than 99.90% of Python3 online submissions for Word Search.
+# Memory Usage: 14.3 MB, less than 21.41% of Python3 online submissions for Word Search.
