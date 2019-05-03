3c34ccd3a19ce993d52f7729927f346e71623099
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-03 21:10:02 +0800

    Create 10. Regular Expression Matching.py

---
+++ 1-100/10. Regular Expression Matching.py
@@ -0,0 +1,66 @@
+"""
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Note:
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like . or *.
+
+Example 1:
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input:
+s = "aa"
+p = "a*"
+Output: true
+Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+Input:
+s = "ab"
+p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Example 4:
+Input:
+s = "aab"
+p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+
+Example 5:
+Input:
+s = "mississippi"
+p = "mis*is*p*."
+Output: false
+"""
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        cache={}
+        def dp(i,j):
+            if (i,j) not in cache:
+                if j==len(p):
+                    ans= i==len(s)
+                else:
+                    first_match= i<len(s) and p[j] in {s[i],'.'}
+                    if j<len(p)-1 and p[j+1]=='*':
+                        ans=dp(i,j+2) or first_match and dp(i+1,j)
+                    else:
+                        ans=first_match and dp(i+1,j+1)
+                cache[i,j]=ans
+            return cache[i,j]
+        
+        return dp(0,0)
+     
+# Runtime: 64 ms, faster than 69.23% of Python3 online submissions for Regular Expression Matching.
+# Memory Usage: 13.5 MB, less than 5.32% of Python3 online submissions for Regular Expression Matching.
