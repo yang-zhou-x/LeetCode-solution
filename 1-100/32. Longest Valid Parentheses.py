594225e188c341b84d680ab354e1b08ff567b0b7
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-08 23:56:59 +0800

    Create 32. Longest Valid Parentheses.py

---
+++ 1-100/32. Longest Valid Parentheses.py
@@ -0,0 +1,29 @@
+'''
+Given a string containing just the characters '(' and ')', 
+find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+
+Example 2:
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+'''
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        dp, stack = [0]*(len(s) + 1), []
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                if stack:
+                    p = stack.pop()
+                    dp[i + 1] = dp[p] + i - p + 1
+        return max(dp)
+        
+# Runtime: 48 ms, faster than 99.57% of Python3 online submissions for Longest Valid Parentheses.
+# Memory Usage: 13.8 MB, less than 5.26% of Python3 online submissions for Longest Valid Parentheses.
