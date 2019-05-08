9209b07663ffa7dc5aea68e09deb08aa73f6d1a7
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-08 22:23:13 +0800

    Create 322. Coin Change.py

---
+++ 301-400/322. Coin Change.py
@@ -0,0 +1,30 @@
+'''
+You are given coins of different denominations and a total amount of money amount. 
+Write a function to compute the fewest number of coins that you need to make up that amount. 
+If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+Input: coins = [1, 2, 5], amount = 11
+Output: 3 
+Explanation: 11 = 5 + 5 + 1
+
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+
+Note:
+You may assume that you have an infinite number of each kind of coin.
+'''
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp=[0]+[float('inf')]*amount
+        
+        # f(amount) = min(f(amount-coin1),f(amount-coin2),...) + 1
+        for idx in range(1,amount+1):
+            dp[idx]=min(dp[idx-c] if idx-c>=0 else float('inf') for c in coins)+1
+        
+        return (dp[-1],-1)[dp[-1]==float('inf')]
+        
+# Runtime: 1088 ms, faster than 75.60% of Python3 online submissions for Coin Change.
+# Memory Usage: 13.3 MB, less than 18.11% of Python3 online submissions for Coin Change.
