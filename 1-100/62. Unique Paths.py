9c7cdd1b982a39b50595d22dcea844bf8f07ee14
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-10 23:43:02 +0800

    Update 62. Unique Paths.py

--- 1-100/62. Unique Paths.py
+++ 1-100/62. Unique Paths.py
@@ -1,10 +1,41 @@
-class Solution:
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-        return math.factorial(m+n-2)//(math.factorial(m-1)*math.factorial(n-1))
+'''
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+How many possible unique paths are there?
 
-# Runtime: 32 ms, faster than 99.23% of Python3 online submissions for Unique Paths.
+Note: m and n will be at most 100.
+
+Example 1:
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Example 2:
+Input: m = 7, n = 3
+Output: 28
+'''
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        from math import factorial as f
+        return f(m + n - 2) // f(m - 1) // f(n - 1)
+# Runtime: 36 ms, faster than 87.68% of Python3 online submissions for Unique Paths.
+# Memory Usage: 13.2 MB, less than 5.25% of Python3 online submissions for Unique Paths.
+
+# 动态规划
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 0 or n == 0:
+            return 0
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j - 1] + dp[j]
+        return dp[-1]
+# Runtime: 36 ms, faster than 87.68% of Python3 online submissions for Unique Paths.
+# Memory Usage: 13.2 MB, less than 5.25% of Python3 online submissions for Unique Paths.
