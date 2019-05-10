55d2b88796ee933d234aafdd2de941d714f31e56
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-10 22:59:29 +0800

    Update 5. Longest Palindromic Substring.py

--- 1-100/5. Longest Palindromic Substring.py
+++ 1-100/5. Longest Palindromic Substring.py
@@ -10,26 +10,27 @@ Example 2:
 Input: "cbbd"
 Output: "bb"
 '''
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand_center(s, left, right):
-            while left>=0 and right<len(s) and s[left]==s[right]:
-                left-=1 # 从中心向外扩展
-                right+=1
-            return right-left-1
-        
         if not s:
             return ''
-        
-        start,end=0,0
+
+        def expand_center(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1  # 从中心向外扩展
+                right += 1
+            return right - left - 1  # 子串长度
+
+        start, end = 0, 0
         for i in range(len(s)):
-            len1=expand_center(s,i,i) # 奇数长度时，即中心在某个字母处
-            len2=expand_center(s,i,i+1) # 偶数长度时，即中心在两个字母之间
-            len12=max(len1,len2)
-            if len12>end-start:
-                start=i-(len12-1)//2
-                end=i+len12//2
-        return s[start:end+1]
+            len1 = expand_center(s, i, i)  # 奇数长度时，即中心在某个字母处
+            len2 = expand_center(s, i, i + 1)  # 偶数长度时，即中心在两个字母之间
+            len12 = max(len1, len2)
+            if len12 > end - start:
+                start = i - (len12 - 1) // 2  # 确定起始位置
+                end = i + len12 // 2
+        return s[start:end + 1]
         
 # Runtime: 892 ms, faster than 75.84% of Python3 online submissions for Longest Palindromic Substring.
 # Memory Usage: 12.9 MB, less than 27.78% of Python3 online submissions for Longest Palindromic Substring.      
