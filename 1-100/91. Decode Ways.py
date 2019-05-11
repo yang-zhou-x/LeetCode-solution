93a0bd75e7a7e258d0c31cc1f8e6a8b858fc7985
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-11 14:04:12 +0800

    Update 91. Decode Ways.py

--- 1-100/91. Decode Ways.py
+++ 1-100/91. Decode Ways.py
@@ -21,17 +21,12 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if not s:
             return 0
-        dp=[0]*(len(s)+1)
-        dp[0]=1
-        # 公式为：
-        # dp[i]=dp[i-1] if s[i-1] != '0'
-        #        + dp[i-2] if '09'<s[i-2:i]<'27'
-        for i in range(1,len(s)+1):
-            if s[i-1]!='0':
-                dp[i]=dp[i-1]
-            if i>1 and '09'<s[i-2:i]<'27':
-                dp[i]+=dp[i-2]
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        for i in range(1, len(dp)):
+            # dp[i] = (dp[i-1] if ...) + (dp[i-2] if ...)
+            dp[i] = (dp[i - 1] if s[i - 1] != '0' else 0) +\
+                (dp[i - 2] if i > 1 and '09' < s[i - 2:i] < '27' else 0)
         return dp[-1]
-        
-# Runtime: 40 ms, faster than 94.30% of Python3 online submissions for Decode Ways.
-# Memory Usage: 13.2 MB, less than 20.73% of Python3 online submissions for Decode Ways.
+# Runtime: 32 ms, faster than 99.79% of Python3 online submissions for Decode Ways.
+# Memory Usage: 13.2 MB, less than 17.46% of Python3 online submissions for Decode Ways.
