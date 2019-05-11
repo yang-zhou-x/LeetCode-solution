a43e56e25ea8f96e6b219e38ab202ef22e866733
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-11 14:59:10 +0800

    Update 121. Best Time to Buy and Sell Stock.py

--- 101-200/121. Best Time to Buy and Sell Stock.py
+++ 101-200/121. Best Time to Buy and Sell Stock.py
@@ -22,14 +22,14 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        buy=float('inf')
-        ans=0
+        buy = float('inf')
+        ans = 0
         for p in prices:
-            if p<buy:
-                buy=p
+            if p < buy:
+                buy = p
             else:
-                if p-buy>ans:
-                    ans=p-buy
+                if p-buy > ans:
+                    ans = p - buy
         return ans
 # Runtime: 20 ms, faster than 100.00% of Python online submissions for Best Time to Buy and Sell Stock.
 # Memory Usage: 11.5 MB, less than 64.80% of Python online submissions for Best Time to Buy and Sell Stock.
