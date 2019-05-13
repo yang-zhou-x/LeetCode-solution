f4fe6d814321eced65526ecb76279feacf8f5093
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-13 17:18:41 +0800

    Update 198. House Robber.py

--- 101-200/198. House Robber.py
+++ 101-200/198. House Robber.py
@@ -1,19 +1,32 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+'''
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed, the only constraint stopping you 
+from robbing each of them is that adjacent houses have security system connected and 
+it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+'''
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        prev=0  # 相当于rob(nums[:-2])
-        now=0  # 相当于rob(nums[:-1])
-        for n in nums:
-            tmp=prev
-            prev=now
-            now=max(tmp+n,now)   # 答案为 rob(nums[:-2])+nums[-1] 和 rob(nums[:-1]) 中较大的那个
-        return now
-        
-
-# Runtime: 20 ms, faster than 84.00% of Python online submissions for House Robber.
-# Memory Usage: 10.7 MB, less than 66.47% of Python online submissions for House Robber.
+        dp = [0] * (len(nums) + 1)
+        dp[1] = nums[0]
+        for i in range(2, len(dp)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])  # 动态规划
+        return dp[-1]
+# Runtime: 32 ms, faster than 98.53% of Python3 online submissions for House Robber.
+# Memory Usage: 13.1 MB, less than 5.00% of Python3 online submissions for House Robber.
