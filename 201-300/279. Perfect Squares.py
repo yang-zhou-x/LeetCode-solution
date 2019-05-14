e734b9aa78b5c080eee7f2b37036df69b8a7f848
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-14 23:49:09 +0800

    Update 279. Perfect Squares.py

--- 201-300/279. Perfect Squares.py
+++ 201-300/279. Perfect Squares.py
@@ -1,3 +1,17 @@
+'''
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+'''
+
 class Solution:
     def numSquares(self, n: int) -> int:
         # BFS算法
@@ -23,7 +37,17 @@ class Solution:
                         break
                     tmp.add(l-s)
             level=tmp  # 更新
-
-
 # Runtime: 240 ms, faster than 76.47% of Python3 online submissions for Perfect Squares.
 # Memory Usage: 13.9 MB, less than 19.01% of Python3 online submissions for Perfect Squares.
+
+
+class Solution:
+    _dp = [0]
+
+    def numSquares(self, n: int) -> int:
+        dp = self._dp
+        while len(dp) <= n:
+            dp += min(dp[-i * i] for i in range(1, int(len(dp)**0.5 + 1))) + 1,
+        return dp[n]
+# Runtime: 112 ms, faster than 91.13% of Python3 online submissions for Perfect Squares.
+# Memory Usage: 13.3 MB, less than 36.50% of Python3 online submissions for Perfect Squares.
