bac2274bf1017e312d90a699b9f28d2526d097dd
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-16 23:02:45 +0800

    Update 322. Coin Change.py

--- 301-400/322. Coin Change.py
+++ 301-400/322. Coin Change.py
@@ -18,13 +18,11 @@ You may assume that you have an infinite number of each kind of coin.
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp=[0]+[float('inf')]*amount
-        
-        # f(amount) = min(f(amount-coin1),f(amount-coin2),...) + 1
-        for idx in range(1,amount+1):
-            dp[idx]=min(dp[idx-c] if idx-c>=0 else float('inf') for c in coins)+1
-        
-        return (dp[-1],-1)[dp[-1]==float('inf')]
-        
-# Runtime: 1088 ms, faster than 75.60% of Python3 online submissions for Coin Change.
-# Memory Usage: 13.3 MB, less than 18.11% of Python3 online submissions for Coin Change.
+        dp = [0] + [float('inf')] * amount
+        # 动态规划：f(amount) = min(f(amount-coin1),f(amount-coin2),...) + 1
+        for idx in range(1, amount+1):
+            dp[idx] = min(dp[idx - c] if idx - c >= 0 else float('inf')
+                          for c in coins) + 1
+        return (dp[-1], -1)[dp[-1] == float('inf')]
+# Runtime: 1032 ms, faster than 78.18% of Python3 online submissions for Coin Change.
+# Memory Usage: 13.2 MB, less than 68.67% of Python3 online submissions for Coin Change.
