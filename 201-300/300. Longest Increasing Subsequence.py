219dfad303980a206c6f002b6550e825797c12a9
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-16 22:46:50 +0800

    Update 300. Longest Increasing Subsequence.py

--- 201-300/300. Longest Increasing Subsequence.py
+++ 201-300/300. Longest Increasing Subsequence.py
@@ -1,25 +1,53 @@
+'''
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+Input: [10,9,2,5,3,7,101,18]
+Output: 4 
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+
+Note:
+There may be more than one LIS combination, it is only necessary for you to return the length.
+Your algorithm should run in O(n2) complexity.
+
+Follow up: Could you improve it to O(n log n) time complexity?
+'''
+
 class Solution:
     def lengthOfLIS(self, nums: 'List[int]') -> 'int':
-        ans=[]
-        for n in nums: #动态规划
-            idx=self.bs(ans,n)
-            if idx==len(ans):
-                ans.append(n) #尾部添加1个数
+        ans = []
+        for n in nums:
+            idx = self.bs(ans, n)
+            if idx == len(ans):
+                ans.append(n)  # 尾部添加1个数
             else:
-                ans[idx]=n #替换掉最小的那个大于（含等于）n的数
+                ans[idx] = n  # 替换掉最小的那个大于（含等于）n的数
         return len(ans)
-    
-    def bs(self,ans,n): #二分查找
-        l,r=0,len(ans)-1
-        while l<=r:
-            m=(l+r)//2
-            if n<ans[m]:
-                r=m-1
-            elif ans[m]<n:
-                l=m+1
+
+    def bs(self, ans, n):  # 二分查找
+        left, right = 0, len(ans)-1
+        while left <= right:
+            mid = (left+right)//2
+            if n < ans[mid]:
+                right = mid-1
+            elif ans[mid] < n:
+                left = mid+1
             else:
-                return m
-        return l
-                
-# Runtime: 60 ms, faster than 73.09% of Python3 online submissions for Longest Increasing Subsequence.
-# Memory Usage: 13.3 MB, less than 5.81% of Python3 online submissions for Longest Increasing Subsequence.
+                return mid
+        return left
+# Runtime: 40 ms, faster than 90.95% of Python3 online submissions for Longest Increasing Subsequence.
+# Memory Usage: 13.2 MB, less than 66.47% of Python3 online submissions for Longest Increasing Subsequence.
+
+# 借助build-in模块
+class Solution:
+    def lengthOfLIS(self, nums: 'List[int]') -> 'int':
+        dp = []
+        for n in nums:
+            idx = bisect.bisect_left(dp, n)
+            if idx < len(dp):
+                dp[idx] = n
+            else:
+                dp.append(n)
+        return len(dp)
+# Runtime: 36 ms, faster than 98.30% of Python3 online submissions for Longest Increasing Subsequence.
+# Memory Usage: 13.3 MB, less than 37.83% of Python3 online submissions for Longest Increasing Subsequence.
