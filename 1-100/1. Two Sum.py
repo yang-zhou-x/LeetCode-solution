4b1b4d5001c9b845d889c1a416ba574c37b5bacd
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-25 15:21:01 +0800

    Update 1. Two Sum.py

--- 1-100/1. Two Sum.py
+++ 1-100/1. Two Sum.py
@@ -1,13 +1,19 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        cache={}
-        for i,n in enumerate(nums):
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        cache = {}
+        for i, n in enumerate(nums):
             if target-n in cache:
-                return [cache[target-n],i]
-            cache[n]=i
-            
+                return [cache[target-n], i]
+            cache[n] = i
+# Runtime: 32 ms, faster than 99.28% of Python3 online submissions for Two Sum.
+# Memory Usage: 14.3 MB, less than 41.10% of Python3 online submissions for Two Sum.
