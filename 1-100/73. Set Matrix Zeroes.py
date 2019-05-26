b30962349f39831dbeb10ee7233d349d34d53954
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-26 14:59:12 +0800

    Update 73. Set Matrix Zeroes.py

--- 1-100/73. Set Matrix Zeroes.py
+++ 1-100/73. Set Matrix Zeroes.py
@@ -1,24 +1,61 @@
+'''
+Given a m x n matrix, if an element is 0, 
+set its entire row and column to 0. Do it in-place.
+
+Example 1:
+Input: 
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+Output: 
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+
+Example 2:
+Input: 
+[
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]
+Output: 
+[
+  [0,0,0,0],
+  [0,4,5,0],
+  [0,3,1,0]
+]
+
+Follow up:
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+'''
+
 class Solution:
-    def setZeroes(self, matrix: 'List[List[int]]') -> 'None':
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         if not matrix or not matrix[0]:
             return
-        row=set()
-        col=set()
+        row = set()
+        col = set()
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j]==0:  # 所有的0的位置
+                if matrix[i][j] == 0:  # 所有的0的位置
                     row.add(i)
                     col.add(j)
         for i in row:
-            matrix[i]=[0]*len(matrix[0])  # 行为0
-            
+            matrix[i] = [0]*len(matrix[0])  # 行为0
+
         for j in col:
             for i in range(len(matrix)):
                 if i not in row:
-                    matrix[i][j]=0  # 列为0
-                    
-# Runtime: 84 ms, faster than 99.48% of Python3 online submissions for Set Matrix Zeroes.
-# Memory Usage: 12.9 MB, less than 50.00% of Python3 online submissions for Set Matrix Zeroes.
+                    matrix[i][j] = 0  # 列为0
+# Runtime: 88 ms, faster than 96.71% of Python3 online submissions for Set Matrix Zeroes.
+# Memory Usage: 13.3 MB, less than 87.69% of Python3 online submissions for Set Matrix Zeroes.
