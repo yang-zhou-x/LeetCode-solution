9dad0eda52f87d9e80e62ae216e6ec130a74048b
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-28 22:28:03 +0800

    Update 167. Two Sum II - Input array is sorted.py

--- 101-200/167. Two Sum II - Input array is sorted.py
+++ 101-200/167. Two Sum II - Input array is sorted.py
@@ -1,3 +1,19 @@
+'''
+Given an array of integers that is already sorted in ascending order, 
+find two numbers such that they add up to a specific target number.
+The function twoSum should return indices of the two numbers such that they add up to the target, 
+where index1 must be less than index2.
+
+Note:
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Example:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+'''
+
 class Solution(object):
     def twoSum(self, numbers, target):
         """
@@ -6,11 +22,9 @@ class Solution(object):
         :rtype: List[int]
         """
         aux={}
-        for i,n in enumerate(numbers,1):
+        for i, n in enumerate(numbers, 1):
             if target-n in aux:
-                return [aux[target-n],i]
-            aux[n]=i
-        
+                return [aux[target-n], i]
+            aux[n] = i
 # Runtime: 16 ms, faster than 100.00% of Python online submissions for Two Sum II - Input array is sorted.
 # Memory Usage: 10.9 MB, less than 53.75% of Python online submissions for Two Sum II - Input array is sorted.
-
