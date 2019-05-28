a435d329d3caa4bb723be5cd5ee5334684afeab4
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-28 22:18:30 +0800

    Update 162. Find Peak Element.py

--- 101-200/162. Find Peak Element.py
+++ 101-200/162. Find Peak Element.py
@@ -1,17 +1,34 @@
+'''
+A peak element is an element that is greater than its neighbors.
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+You may imagine that nums[-1] = nums[n] = -∞.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+
+Example 2:
+Input: nums = [1,2,1,3,5,6,4]
+Output: 1 or 5 
+Explanation: Your function can return either index number 1 where the peak element is 2, 
+             or index number 5 where the peak element is 6.
+
+Note:
+Your solution should be in logarithmic complexity.
+'''
+
 class Solution:
-    def findPeakElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        l,r=0,len(nums)-1
-        while l<r:
-            m=(l+r)//2  # 因为只需要返回1个peak，所以可以使用二分法查找。也可以顺序查找。
-            if nums[m]>nums[m+1]:
-                r=m  # 包含右侧点
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+        while l < r:
+            m = (l+r) >> 1
+            # 因为只需要返回1个peak，所以可以使用二分法查找。也可以顺序查找。
+            if nums[m] > nums[m+1]:
+                r = m  # 包含右侧点
             else:
-                l=m+1
-        
+                l = m+1
         return l
-        
-# Runtime: 32 ms, faster than 99.43% of Python3 online submissions for Find Peak Element.
+# Runtime: 28 ms, faster than 99.30% of Python3 online submissions for Find Peak Element.
+# Memory Usage: 13.2 MB, less than 62.72% of Python3 online submissions for Find Peak Element.
