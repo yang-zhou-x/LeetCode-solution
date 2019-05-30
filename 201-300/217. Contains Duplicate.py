5b49ff0f77fc84a2e3e9cb350ff6261ab6f9f9da
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-30 23:29:58 +0800

    Update 217. Contains Duplicate.py

--- 201-300/217. Contains Duplicate.py
+++ 201-300/217. Contains Duplicate.py
@@ -1,13 +1,28 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        aux=set()
+'''
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
+
+Example 1:
+Input: [1,2,3,1]
+Output: true
+
+Example 2:
+Input: [1,2,3,4]
+Output: false
+
+Example 3:
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+'''
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        cache = set()
         for n in nums:
-            if n in aux:
+            if n in cache:
                 return True
-            aux.add(n)
+            cache.add(n)
         return False
-        
+# Runtime: 28 ms, faster than 99.98% of Python3 online submissions for Contains Duplicate.
+# Memory Usage: 18.9 MB, less than 42.33% of Python3 online submissions for Contains Duplicate.
