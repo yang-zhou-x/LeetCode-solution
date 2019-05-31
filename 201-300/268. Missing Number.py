5fba1561b9bb482c0c99342ce1f0519e6751df3e
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-31 22:43:28 +0800

    Update 268. Missing Number.py

--- 201-300/268. Missing Number.py
+++ 201-300/268. Missing Number.py
@@ -1,12 +1,23 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        tot=(1+len(nums))*len(nums)//2
-        return tot-sum(nums)
+'''
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+find the one that is missing from the array.
 
-# Runtime: 24 ms, faster than 97.85% of Python online submissions for Missing Number.
-# Memory Usage: 11.5 MB, less than 23.23% of Python online submissions for Missing Number.
- 
+Example 1:
+Input: [3,0,1]
+Output: 2
+
+Example 2:
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+
+Note:
+Your algorithm should run in linear runtime complexity. 
+Could you implement it using only constant extra space complexity?
+'''
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        expect = (1+len(nums))*len(nums) >> 1
+        return expect-sum(nums)
+# Runtime: 40 ms, faster than 97.05% of Python3 online submissions for Missing Number.
+# Memory Usage: 14.2 MB, less than 34.23% of Python3 online submissions for Missing Number.
