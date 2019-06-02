e43670751b6422b65550dace21d360a32fd04977
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-02 13:32:44 +0800

    Update 380. Insert Delete GetRandom O(1).py

--- 301-400/380. Insert Delete GetRandom O(1).py
+++ 301-400/380. Insert Delete GetRandom O(1).py
@@ -1,52 +1,28 @@
-class RandomizedSet:
+'''
+Design a data structure that supports all following operations in average O(1) time.
 
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.nums=[]
-        self.posi={}  # {val:索引}
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
 
-    def insert(self, val):
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        :type val: int
-        :rtype: bool
-        """
-        if val not in self.posi:
-            self.nums.append(val)
-            self.posi[val]=len(self.nums)-1
-            return True
-        else:
-            return False
-
-    def remove(self, val):
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        :type val: int
-        :rtype: bool
-        """
-        if val in self.posi:
-            idx=self.posi[val]
-            last=self.nums[-1]
-            # 将val替换为last后，直接删去原位置的last
-            self.nums[idx]=last
-            self.posi[last]=idx
-            # remove
-            self.nums.pop()
-            self.posi.pop(val)
-            return True
-        else:
-            return False
-        
-
-    def getRandom(self):
-        """
-        Get a random element from the set.
-        :rtype: int
-        """
-        return random.choice(self.nums)
-
+Example:
+// Init an empty set.
+RandomizedSet randomSet = new RandomizedSet();
+// Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomSet.insert(1);
+// Returns false as 2 does not exist in the set.
+randomSet.remove(2);
+// Inserts 2 to the set, returns true. Set now contains [1,2].
+randomSet.insert(2);
+// getRandom should return either 1 or 2 randomly.
+randomSet.getRandom();
+// Removes 1 from the set, returns true. Set now contains [2].
+randomSet.remove(1);
+// 2 was already in the set, so return false.
+randomSet.insert(2);
+// Since 2 is the only number in the set, getRandom always return 2.
+randomSet.getRandom();
+'''
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
@@ -54,5 +30,50 @@ class RandomizedSet:
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
 
-# Runtime: 96 ms, faster than 74.46% of Python3 online submissions for Insert Delete GetRandom O(1).
-# Memory Usage: 16.4 MB, less than 5.30% of Python3 online submissions for Insert Delete GetRandom O(1).
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = []
+        self.posi = {}  # {值:索引}
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already
+        contain the specified element.
+        """
+        if val not in self.posi:
+            self.nums.append(val)
+            self.posi[val] = len(self.nums)-1
+            return True
+        else:
+            return False
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained
+        the specified element.
+        """
+        if val in self.posi:
+            idx = self.posi[val]
+            last = self.nums[-1]
+            # 将val替换为last后，直接删去原位置的last
+            self.nums[idx] = last
+            self.posi[last] = idx
+            # remove
+            self.nums.pop()
+            self.posi.pop(val)
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return random.choice(self.nums)
+
+# Runtime: 88 ms, faster than 99.15% of Python3 online submissions for Insert Delete GetRandom O(1).
+# Memory Usage: 16.9 MB, less than 48.41% of Python3 online submissions for Insert Delete GetRandom O(1).
