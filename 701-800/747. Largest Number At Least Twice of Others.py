599fad5f4ea5cbfa3cb117326da736b5cab0d6e2
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-03 15:56:07 +0800

    Update 747. Largest Number At Least Twice of Others.py

--- 701-800/747. Largest Number At Least Twice of Others.py
+++ 701-800/747. Largest Number At Least Twice of Others.py
@@ -1,23 +1,36 @@
-class Solution(object):
-    def dominantIndex(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        idx=0
-        maxi=nums[0]
-        for i,n in enumerate(nums):
-            if n>maxi:
-                maxi=n
-                idx=i
-        index=[x for x in range(len(nums)) if x!=idx]
-        for i in index:
+'''
+In a given integer array nums, there is always exactly one largest element.
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+If it is, return the index of the largest element, otherwise return -1.
+
+Example 1:
+Input: nums = [3, 6, 1, 0]
+Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
+Example 2:
+Input: nums = [1, 2, 3, 4]
+Output: -1
+Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ 
+Note:
+nums will have a length in the range [1, 50].
+Every nums[i] will be an integer in the range [0, 99].
+'''
+
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        maxi = max(nums)
+        maxi_idx = nums.index(maxi)
+        for i, n in enumerate(nums):
+            if i == maxi_idx:
+                continue
             try:
-                if maxi/nums[i]<2:
+                if maxi/n < 2:
                     return -1
             except ZeroDivisionError:
                 pass
-        return idx
-        
-# Runtime: 20 ms, faster than 97.98% of Python online submissions for Largest Number At Least Twice of Others.
-# Memory Usage: 10.7 MB, less than 70.00% of Python online submissions for Largest Number At Least Twice of Others.
+        return maxi_idx
+# Runtime: 28 ms, faster than 99.48% of Python3 online submissions for Largest Number At Least Twice of Others.
+# Memory Usage: 13.2 MB, less than 37.41% of Python3 online submissions for Largest Number At Least Twice of Others.
