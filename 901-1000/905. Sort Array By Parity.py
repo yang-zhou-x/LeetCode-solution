74cbe29f94ee309e35f7520ecdeb9d5bc08fa2cb
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-04 22:50:34 +0800

    Update 905. Sort Array By Parity.py

--- 901-1000/905. Sort Array By Parity.py
+++ 901-1000/905. Sort Array By Parity.py
@@ -1,11 +1,28 @@
-class Solution(object):
-    def sortArrayByParity(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
-        even=[]
-        odd=[]
-        [even.append(x) if x%2==0 else odd.append(x) for x in A]
+'''
+Given an array A of non-negative integers, 
+return an array consisting of all the even elements of A, 
+followed by all the odd elements of A.
+You may return any answer array that satisfies this condition.
+
+Example 1:
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ 
+Note:
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+'''
+
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        even = []
+        odd = []
+        for a in A:
+            if a % 2 == 0:
+                even.append(a)
+            else:
+                odd.append(a)
         return even+odd
-        
+# Runtime: 60 ms, faster than 98.81% of Python3 online submissions for Sort Array By Parity.
+# Memory Usage: 13.9 MB, less than 10.15% of Python3 online submissions for Sort Array By Parity.
