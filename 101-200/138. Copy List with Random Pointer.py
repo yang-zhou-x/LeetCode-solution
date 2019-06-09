2243acbfe932a6f11b0dc1873aa38aab500b0028
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-09 23:03:39 +0800

    Update 138. Copy List with Random Pointer.py

--- 101-200/138. Copy List with Random Pointer.py
+++ 101-200/138. Copy List with Random Pointer.py
@@ -1,3 +1,19 @@
+'''
+A linked list is given such that each node contains an additional random pointer 
+which could point to any node in the list or null.
+Return a deep copy of the list.
+
+Example 1:
+Input:
+{"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}
+Explanation:
+Node 1's value is 1, both of its next and random pointer points to Node 2.
+Node 2's value is 2, its next pointer points to null and its random pointer points to itself.
+
+Note:
+You must return the copy of the given head as a reference to the cloned list.
+'''
+
 """
 # Definition for a Node.
 class Node:
@@ -8,7 +24,7 @@ class Node:
 """
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        dic = collections.defaultdict(lambda: Node(0,0,0))
+        dic = collections.defaultdict(lambda: Node(0, 0, 0))
         dic[None] = None
         p = head
         while p:
@@ -17,6 +33,5 @@ class Solution:
             dic[p].random = dic[p.random]
             p = p.next
         return dic[head]
-        
 # Runtime: 48 ms, faster than 100.00% of Python3 online submissions for Copy List with Random Pointer.
 # Memory Usage: 15.5 MB, less than 100.00% of Python3 online submissions for Copy List with Random Pointer.
