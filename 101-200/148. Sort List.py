689052b1157a4b091c8db7d74f97b51a3e201f04
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-12 22:52:52 +0800

    Update 148. Sort List.py

--- 101-200/148. Sort List.py
+++ 101-200/148. Sort List.py
@@ -1,3 +1,15 @@
+'''
+Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+Input: 4->2->1->3
+Output: 1->2->3->4
+
+Example 2:
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -6,19 +18,16 @@
 
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
-        if not head:
-            return
-        p=head
-        nums=[]
+        nums = []
+        p = head
         while p:
             nums.append(p.val)
-            p=p.next
+            p = p.next
         nums.sort()
-        p=head
+        p = head
         for n in nums:
-            p.val=n
-            p=p.next
+            p.val = n
+            p = p.next
         return head
-        
-# Runtime: 88 ms, faster than 99.08% of Python3 online submissions for Sort List.
-# Memory Usage: 20.3 MB, less than 16.07% of Python3 online submissions for Sort List.
+# Runtime: 80 ms, faster than 99.91% of Python3 online submissions for Sort List.
+# Memory Usage: 20.5 MB, less than 91.55% of Python3 online submissions for Sort List.
