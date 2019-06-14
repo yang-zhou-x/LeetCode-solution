3bdf4a2d5a95f53a2323632e84ec0a4c3b598dfb
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-14 16:02:41 +0800

    Update 647. Palindromic Substrings.py

--- 601-700/647. Palindromic Substrings.py
+++ 601-700/647. Palindromic Substrings.py
@@ -38,3 +38,27 @@ class Solution:
         return count
 # Runtime: 136 ms, faster than 68.40% of Python3 online submissions for Palindromic Substrings.
 # Memory Usage: 13 MB, less than 85.04% of Python3 online submissions for Palindromic Substrings.
+
+# 另一种写法
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        if not s:
+            return 0
+
+        def expand_center(left, right):
+            count = 0
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    count += 1
+                    left -= 1
+                    right += 1
+                else:
+                    return count
+            return count
+        ans = 0
+        for i in range(len(s)):
+            ans += expand_center(i, i)
+            ans += expand_center(i, i+1)
+        return ans
+# Runtime: 132 ms, faster than 74.64% of Python3 online submissions for Palindromic Substrings.
+# Memory Usage: 13.1 MB, less than 75.02% of Python3 online submissions for Palindromic Substrings.
