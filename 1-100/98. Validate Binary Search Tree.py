a2d84e94ba1367b51c33e6ae7257243d9e026286
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-15 12:57:18 +0800

    Update 98. Validate Binary Search Tree.py

--- 1-100/98. Validate Binary Search Tree.py
+++ 1-100/98. Validate Binary Search Tree.py
@@ -1,45 +1,67 @@
+'''
+Given a binary tree, determine if it is a valid binary search tree (BST).
+Assume a BST is defined as follows:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+    2
+   / \
+  1   3
+Input: [2,1,3]
+Output: true
+
+Example 2:
+    5
+   / \
+  1   4
+     / \
+    3   6
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
+# 解法1，递归
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        if not root:
-            return True
-        nums=[]
-        def in_order(node):  # inorder顺序输出
-            if node:
-                in_order(node.left)
-                nonlocal nums
-                nums.append(node.val)
-                in_order(node.right)
-        in_order(root)
-        for i in range(len(nums)-1):
-            if nums[i]>=nums[i+1]:
+        nums = []
+
+        def dfs(root):  # inorder遍历
+            if root:
+                dfs(root.left)
+                nums.append(root.val)
+                dfs(root.right)
+        dfs(root)
+        for i in range(1, len(nums)):
+            if nums[i-1] >= nums[i]:
                 return False
         return True
+# Runtime: 52 ms, faster than 84.56% of Python3 online submissions for Validate Binary Search Tree.
+# Memory Usage: 16.4 MB, less than 9.06% of Python3 online submissions for Validate Binary Search Tree.
 
-# 简化版本，也为inorder思路：
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        stack=[]
-        small=float('-inf')
+# 解法2，迭代，也为inorder思路，但可以提前终止，不遍历整个树
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        stack = []
+        small = float('-inf')
         while stack or root:
             while root:
                 stack.append(root)
-                root=root.left
-            root=stack.pop()
-            if root.val<=small:
+                root = root.left
+            root = stack.pop()
+            if root.val <= small:
                 return False
-            small=root.val
-            root=root.right
+            small = root.val
+            root = root.right
         return True
-     
-# Runtime: 36 ms, faster than 94.74% of Python online submissions for Validate Binary Search Tree.
-# Memory Usage: 16.1 MB, less than 6.88% of Python online submissions for Validate Binary Search Tree.
+# Runtime: 44 ms, faster than 98.36% of Python3 online submissions for Validate Binary Search Tree.
+# Memory Usage: 15.5 MB, less than 94.70% of Python3 online submissions for Validate Binary Search Tree.
