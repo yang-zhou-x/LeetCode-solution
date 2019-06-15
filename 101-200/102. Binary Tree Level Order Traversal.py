2e59135b513a1604f93f783ebee58a2e1f76f32d
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-15 16:26:13 +0800

    Update 102. Binary Tree Level Order Traversal.py

--- 101-200/102. Binary Tree Level Order Traversal.py
+++ 101-200/102. Binary Tree Level Order Traversal.py
@@ -1,3 +1,21 @@
+'''
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -11,13 +29,12 @@ class Solution:
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        ans=[]
+        ans = []
         if root:
-            level=[root]
+            level = [root]
             while level:
                 ans.append([node.val for node in level])
-                lrPair=[(node.left,node.right) for node in level]
-                level=[node for lr in lrPair for node in lr if node]
+                lrPair = [(node.left, node.right) for node in level]
+                level = [node for lr in lrPair for node in lr if node]
         return ans
-
 # Runtime: 40 ms, faster than 96.15% of Python3 online submissions for Binary Tree Level Order Traversal.
