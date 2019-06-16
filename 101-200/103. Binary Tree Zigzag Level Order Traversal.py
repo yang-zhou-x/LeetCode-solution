fab749d9771fee08fd4fd0061fd68668c6217e51
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-16 15:46:49 +0800

    Update 103. Binary Tree Zigzag Level Order Traversal.py

--- 101-200/103. Binary Tree Zigzag Level Order Traversal.py
+++ 101-200/103. Binary Tree Zigzag Level Order Traversal.py
@@ -1,3 +1,22 @@
+'''
+Given a binary tree, return the zigzag level order traversal of its nodes' values. 
+(ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -9,18 +28,36 @@ class Solution:
     def zigzagLevelOrder(self, root: 'TreeNode') -> 'List[List[int]]':
         if not root:
             return []
-        
-        level=[root]
-        ans=[]
-        fromRight=False
+        level = [root]
+        ans = []
+        fromRight = False
         while level:
-            vals=[node.val for node in level]
+            vals = [node.val for node in level]
             if fromRight:
                 vals.reverse()
             ans.append(vals)
-            fromRight=not fromRight
-            lrPairs=[(node.left,node.right) for node in level]
-            level=[node for lr in lrPairs for node in lr if node]
+            fromRight = not fromRight
+            lrPairs = [(node.left, node.right) for node in level]
+            level = [node for lr in lrPairs for node in lr if node]
         return ans
-        
 # Runtime: 36 ms, faster than 99.52% of Python3 online submissions for Binary Tree Zigzag Level Order Traversal.
+
+# 2019/6/16更新，没啥区别
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        ans = []
+        fromRight = False
+        level = [root]
+        while level:
+            val = [node.val for node in level]
+            if fromRight:
+                val.reverse()
+            ans.append(val)
+            fromRight = not fromRight
+            pairs = [(node.left, node.right) for node in level]
+            level = [node for pair in pairs for node in pair if node]
+        return ans
+# Runtime: 32 ms, faster than 98.46% of Python3 online submissions for Binary Tree Zigzag Level Order Traversal.
+# Memory Usage: 13.7 MB, less than 5.01% of Python3 online submissions for Binary Tree Zigzag Level Order Traversal.
