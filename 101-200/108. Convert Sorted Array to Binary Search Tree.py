f0da3d7157c190c844ca1330917cafda4d65570b
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-21 13:37:07 +0800

    Update 108. Convert Sorted Array to Binary Search Tree.py

--- 101-200/108. Convert Sorted Array to Binary Search Tree.py
+++ 101-200/108. Convert Sorted Array to Binary Search Tree.py
@@ -1,3 +1,18 @@
+'''
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+For this problem, a height-balanced binary tree is defined as a binary tree 
+in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+Given the sorted array: [-10,-3,0,5,9],
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+ '''
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -5,23 +20,18 @@
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def sortedArrayToBST(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: TreeNode
-        """
-        def aux(left,right):
-            if left<=right:
-                mid=(left+right)//2
-                root=TreeNode(nums[mid])
-                root.left=aux(left,mid-1)
-                root.right=aux(mid+1,right)
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def help(left, right):
+            if left <= right:
+                mid = (left+right) >> 1
+                root = TreeNode(nums[mid])
+                root.left = help(left, mid-1)
+                root.right = help(mid+1, right)
+                return root
             else:
                 return None
-            return root
-        return aux(0,len(nums)-1)
+        return help(0, len(nums)-1)
         
-# Runtime: 44 ms, faster than 99.63% of Python online submissions for Convert Sorted Array to Binary Search Tree.
-# Memory Usage: 14.8 MB, less than 61.85% of Python online submissions for Convert Sorted Array to Binary Search Tree.
- 
+# Runtime: 60 ms, faster than 93.07% of Python3 online submissions for Convert Sorted Array to Binary Search Tree.
+# Memory Usage: 15.2 MB, less than 99.44% of Python3 online submissions for Convert Sorted Array to Binary Search Tree.
