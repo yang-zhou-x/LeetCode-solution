9dd9d1756c88be6f6f77e099e24a4580990c094a
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-22 18:39:55 +0800

    Update 226. Invert Binary Tree.py

--- 201-300/226. Invert Binary Tree.py
+++ 201-300/226. Invert Binary Tree.py
@@ -1,3 +1,26 @@
+'''
+Invert a binary tree.
+
+Example:
+Input:
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), 
+but you can’t invert a binary tree on a whiteboard so f*** off.
+'''
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -5,6 +28,7 @@
 #         self.left = None
 #         self.right = None
 
+# python2
 class Solution(object):
     def invertTree(self, root):
         """
@@ -12,12 +36,22 @@ class Solution(object):
         :rtype: TreeNode
         """
         if root:
-            tmp=root.left
-            root.left=root.right
-            root.right=tmp
+            tmp = root.left
+            root.left = root.right
+            root.right = tmp
             self.invertTree(root.left)
             self.invertTree(root.right)
         return root
-            
 # Runtime: 16 ms, faster than 100.00% of Python online submissions for Invert Binary Tree.
 # Memory Usage: 10.7 MB, less than 77.97% of Python online submissions for Invert Binary Tree.
+
+# python3
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            tmp = root.left
+            root.left = self.invertTree(root.right)
+            root.right = self.invertTree(tmp)
+        return root
+# Runtime: 32 ms, faster than 92.60% of Python3 online submissions for Invert Binary Tree.
+# Memory Usage: 13.1 MB, less than 80.98% of Python3 online submissions for Invert Binary Tree.
