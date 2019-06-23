d438469690246227ae47c4c7efba4c781271e6c6
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-23 16:19:05 +0800

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- 201-300/236. Lowest Common Ancestor of a Binary Tree.py
+++ 201-300/236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,3 +1,24 @@
+'''
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+According to the definition of LCA on Wikipedia: 
+“The lowest common ancestor is defined between two nodes p and q as the lowest node in T 
+that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+
+Example 2:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+
+Note:
+All of the nodes' values will be unique.
+p and q are different and both values will exist in the binary tree.
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -7,25 +28,22 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        stack=[root]
-        parent={root:None}  # 用来记录节点的父节点
+        stack = [root]
+        parent = {root: None}  # 用来记录p、q的所有父节点
         while p not in parent or q not in parent:
-            node=stack.pop()
+            node = stack.pop()
             if node.left:
-                parent[node.left]=node
+                parent[node.left] = node
                 stack.append(node.left)
             if node.right:
-                parent[node.right]=node
+                parent[node.right] = node
                 stack.append(node.right)
-        
-        ance=set()  # 存放p的父节点路径
+        ance = set()  # 存放p的父节点路径
         while p:
             ance.add(p)
-            p=parent[p]
+            p = parent[p]
         while q not in ance:  # 在p的父节点中寻找
-            q=parent[q]
-            
+            q = parent[q]
         return q
-        
 # Runtime: 80 ms, faster than 93.69% of Python3 online submissions for Lowest Common Ancestor of a Binary Tree.
 # Memory Usage: 17.2 MB, less than 80.07% of Python3 online submissions for Lowest Common Ancestor of a Binary Tree.
