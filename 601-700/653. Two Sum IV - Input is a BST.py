921a139e54757d369b8c55446839f2faf81a620d
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-06-26 13:28:49 +0800

    Update 653. Two Sum IV - Input is a BST.py

--- 601-700/653. Two Sum IV - Input is a BST.py
+++ 601-700/653. Two Sum IV - Input is a BST.py
@@ -1,3 +1,28 @@
+'''
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST 
+such that their sum is equal to the given target.
+
+Example 1:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+Target = 9
+Output: True
+ 
+Example 2:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+Target = 28
+Output: False
+'''
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -27,3 +52,22 @@ class Solution(object):
                 stack.append(node.right)
         return False
         
+# python3
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        if not root:
+            return False
+        stack = [root]
+        cache = set()
+        while stack:
+            node = stack.pop()
+            if k-node.val in cache:
+                return True
+            cache.add(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return False
+# Runtime: 84 ms, faster than 85.52% of Python3 online submissions for Two Sum IV - Input is a BST.
+# Memory Usage: 15.7 MB, less than 27.09% of Python3 online submissions for Two Sum IV - Input is a BST.
