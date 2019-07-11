2dc72ecde125a25209426049bbe3be53425c38a6
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-11 13:47:14 +0800

    Update 50. Pow(x, n).py

--- 1-100/50. Pow(x, n).py
+++ 1-100/50. Pow(x, n).py
@@ -1,3 +1,24 @@
+'''
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+Input: 2.00000, 10
+Output: 1024.00000
+
+Example 2:
+Input: 2.10000, 3
+Output: 9.26100
+
+Example 3:
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+Note:
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+'''
+
 class Solution(object):
     def myPow(self, x, n):
         """
@@ -13,6 +34,18 @@ class Solution(object):
             return x*self.myPow(x,n-1)
         else:
             return self.myPow(x*x,n/2)
-            
 # Runtime: 20 ms, faster than 96.13% of Python online submissions for Pow(x, n).
 # Memory Usage: 10.8 MB, less than 38.57% of Python online submissions for Pow(x, n).
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 1/self.myPow(x, -n)
+        elif n % 2:
+            return x*self.myPow(x, n-1)
+        else:
+            return self.myPow(x*x, n/2)
+# Runtime: 36 ms, faster than 69.73% of Python3 online submissions for Pow(x, n).
+# Memory Usage: 13.3 MB, less than 41.09% of Python3 online submissions for Pow(x, n).
