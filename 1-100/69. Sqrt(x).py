dfecb9cf5bf72694d4d300b4997b88d1ac61676f
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-11 13:54:02 +0800

    Update 69. Sqrt(x).py

--- 1-100/69. Sqrt(x).py
+++ 1-100/69. Sqrt(x).py
@@ -1,3 +1,20 @@
+'''
+Implement int sqrt(int x).
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+Since the return type is an integer, 
+the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+Input: 4
+Output: 2
+
+Example 2:
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.
+'''
+
 class Solution(object):
     def mySqrt(self, x):
         """
@@ -8,6 +25,14 @@ class Solution(object):
         while r*r>x:
             r=(r+x/r)//2
         return r
-        
 # Runtime: 28 ms, faster than 96.64% of Python online submissions for Sqrt(x).
 # Memory Usage: 11 MB, less than 5.37% of Python online submissions for Sqrt(x).
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        r = x
+        while r * r > x:
+            r = (r + x / r) // 2  # 牛顿法
+        return int(r)
+# Runtime: 40 ms, faster than 79.21% of Python3 online submissions for Sqrt(x).
+# Memory Usage: 13.1 MB, less than 86.43% of Python3 online submissions for Sqrt(x).
