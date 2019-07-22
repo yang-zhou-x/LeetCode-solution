02cc50a9f93d91b9df503c8df705301fc77ab34d
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-22 22:25:36 +0800

    Update 3. Longest Substring Without Repeating Characters.py

--- 1-100/3. Longest Substring Without Repeating Characters.py
+++ 1-100/3. Longest Substring Without Repeating Characters.py
@@ -1,3 +1,23 @@
+'''
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+
+Example 2:
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
@@ -15,6 +35,24 @@ class Solution(object):
                 ans=max(ans,i-start+1)
                 cache[ss]=i # 添加位置
         return ans
-        
 # Runtime: 44 ms, faster than 99.14% of Python online submissions for Longest Substring Without Repeating Characters.
 # Memory Usage: 11.2 MB, less than 49.60% of Python online submissions for Longest Substring Without Repeating Characters.
+
+# 2019/07/22
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) < 2:
+            return len(s)
+        pos = {}  # 用来记录每个字母最近一次出现的位置
+        start = 0
+        ans = 0
+        for i, ss in enumerate(s):
+            if ss in pos and start <= pos[ss]:
+                ans = max(ans, i - start)
+                start = pos[ss] + 1  # 用来记录每个字母最近一次出现的位置
+                pos[ss] = i  # 更新ss的位置
+            else:
+                pos[ss] = i  # 添加位置
+        return max(ans, i - start + 1)
+# Runtime: 52 ms, faster than 97.99% of Python3 online submissions for Longest Substring Without Repeating Characters.
+# Memory Usage: 13.9 MB, less than 5.03% of Python3 online submissions for Longest Substring Without Repeating Characters.
