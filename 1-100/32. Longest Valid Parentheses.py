a7701198c64c427880770fc9ca65874a5d276066
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-23 21:50:15 +0800

    Update 32. Longest Valid Parentheses.py

--- 1-100/32. Longest Valid Parentheses.py
+++ 1-100/32. Longest Valid Parentheses.py
@@ -22,9 +22,28 @@ class Solution:
                 stack.append(i)
             else:
                 if stack:
-                    p = stack.pop()  # '('的索引进入stack
+                    p = stack.pop()  # 最近'('的索引
                     # 当前位置的值 = '('前紧邻的合格字串长度 + 当前合格的字串长度
                     dp[i + 1] = dp[p] + i - p + 1
         return max(dp)
 # Runtime: 44 ms, faster than 99.39% of Python3 online submissions for Longest Valid Parentheses.
 # Memory Usage: 13.7 MB, less than 42.67% of Python3 online submissions for Longest Valid Parentheses.
+
+# 20190723
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+        dp = [0] * (len(s) + 1)
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                if stack:
+                    pos = stack.pop()  # 在当前位置之前，最近的一个'('
+                    # 当前位置 = pos之前的长度 + 当前段的长度
+                    dp[i + 1] = dp[pos] + i - pos + 1
+        return max(dp)
+# Runtime: 48 ms, faster than 85.95% of Python3 online submissions for Longest Valid Parentheses.
+# Memory Usage: 14.3 MB, less than 7.51% of Python3 online submissions for Longest Valid Parentheses.
