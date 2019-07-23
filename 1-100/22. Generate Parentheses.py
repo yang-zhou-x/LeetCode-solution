c5541bc8ad69af9ea718faa046d580061f13f2cd
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-23 21:20:53 +0800

    Update 22. Generate Parentheses.py

--- 1-100/22. Generate Parentheses.py
+++ 1-100/22. Generate Parentheses.py
@@ -1,3 +1,16 @@
+'''
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+'''
+
 class Solution:
     def generateParenthesis(self, n):
         """
@@ -14,7 +27,26 @@ class Solution:
             if r<l:
                 f(s+')',l,r+1)
         f()
-        
         return ans 
-        
 # Runtime: 36 ms, faster than 99.65% of Python3 online submissions for Generate Parentheses.
+
+# 20190723
+class Solution:
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+
+        def helper(path, left, right):
+            if len(path) == n * 2:
+                ans.append(path)
+            if left < n:
+                helper(path + '(', left + 1, right)
+            if right < left:
+                helper(path + ')', left, right + 1)
+        helper('', 0, 0)
+        return ans
+# Runtime: 40 ms, faster than 79.33% of Python3 online submissions for Generate Parentheses.
+# Memory Usage: 14.1 MB, less than 5.01% of Python3 online submissions for Generate Parentheses.
