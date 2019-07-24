f636db32c5fda2eb8010673e80ef5e254974e740
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-24 21:34:55 +0800

    Create 67. Add Binary.py

---
+++ 1-100/67. Add Binary.py
@@ -0,0 +1,25 @@
+'''
+Given two binary strings, return their sum (also a binary string).
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+'''
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        if not a or not b:
+            return a or b
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1') + '0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1], b[:-1]) + '0'
+        else:
+            return self.addBinary(a[:-1], b[:-1]) + '1'
+# Runtime: 52 ms, faster than 8.46% of Python3 online submissions for Add Binary.
+# Memory Usage: 14.1 MB, less than 5.09% of Python3 online submissions for Add Binary.
