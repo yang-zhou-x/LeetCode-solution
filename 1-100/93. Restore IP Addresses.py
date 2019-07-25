8e0605e520cb5bc68eab67516f5b1b39661f3691
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-26 00:04:54 +0800

    Create 93. Restore IP Addresses.py

---
+++ 1-100/93. Restore IP Addresses.py
@@ -0,0 +1,11 @@
+'''
+Given a string containing only digits, 
+restore it by returning all possible valid IP address combinations.
+
+Example:
+Input: "25525511135"
+Output: ["255.255.11.135", "255.255.111.35"]
+'''
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
