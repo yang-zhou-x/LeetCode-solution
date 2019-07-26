e38ccdb34f4af7c213bb7092d25af5d1b8f5a7af
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-26 20:50:26 +0800

    Create 165. Compare Version Numbers.py

---
+++ 101-200/165. Compare Version Numbers.py
@@ -0,0 +1,66 @@
+'''
+Compare two version numbers version1 and version2.
+If version1 > version2 return 1; if version1 < version2 return -1;otherwise return 0.
+You may assume that the version strings are non-empty and contain only digits and the . character.
+The . character does not represent a decimal point and is used to separate number sequences.
+For instance, 2.5 is not "two and a half" or "half way to version three", 
+it is the fifth second-level revision of the second first-level revision.
+You may assume the default revision number for each level of a version number to be 0. 
+For example, version number 3.4 has a revision number of 3 and 4 for its first and second level revision number. 
+Its third and fourth level revision number are both 0.
+
+Example 1:
+Input: version1 = "0.1", version2 = "1.1"
+Output: -1
+
+Example 2:
+Input: version1 = "1.0.1", version2 = "1"
+Output: 1
+
+Example 3:
+Input: version1 = "7.5.2.4", version2 = "7.5.3"
+Output: -1
+
+Example 4:
+Input: version1 = "1.01", version2 = "1.001"
+Output: 0
+Explanation: Ignoring leading zeroes, both “01” and “001" represent the same number “1”
+
+Example 5:
+Input: version1 = "1.0", version2 = "1.0.0"
+Output: 0
+Explanation: The first version number does not have a third level revision number, which means its third level revision number is default to "0"
+ 
+
+Note:
+Version strings are composed of numeric strings separated by dots . and this numeric strings may have leading zeroes.
+Version strings do not start or end with dots, and they will not be two consecutive dots.
+'''
+
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        l1 = version1.split('.')
+        l2 = version2.split('.')
+        left1, left2 = 0, 0
+        while left1 < len(l1) and left2 < len(l2):  # 相同位置的比较
+            if int(l1[left1]) > int(l2[left2]):
+                return 1
+            elif int(l1[left1]) < int(l2[left2]):
+                return -1
+            else:
+                left1 += 1
+                left2 += 1
+        if left1 == len(l1) and left2 == len(l2):
+            return 0
+        if left1 < len(l1):  # 剩余位置的比较
+            for l in l1[left1:]:
+                if int(l) != 0:
+                    return 1
+            return 0
+        if left2 < len(l2):
+            for l in l2[left2:]:
+                if int(l) != 0:
+                    return -1
+            return 0
+# Runtime: 28 ms, faster than 97.69% of Python3 online submissions for Compare Version Numbers.
+# Memory Usage: 13.8 MB, less than 5.39% of Python3 online submissions for Compare Version Numbers.
