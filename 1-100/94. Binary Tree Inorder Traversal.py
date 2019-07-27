5b903fd28346787bf9a0c53b1cc896933bc9a20e
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-27 20:06:50 +0800

    Update 94. Binary Tree Inorder Traversal.py

--- 1-100/94. Binary Tree Inorder Traversal.py
+++ 1-100/94. Binary Tree Inorder Traversal.py
@@ -25,3 +25,20 @@ class Solution:
         return nums
 # Runtime: 32 ms, faster than 94.21% of Python3 online submissions for Binary Tree Inorder Traversal.
 # Memory Usage: 13.2 MB, less than 28.91% of Python3 online submissions for Binary Tree Inorder Traversal.
+
+# 20190727
+# iteration, using stack
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ans =[]
+        stack =[]
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            ans.append(root.val)
+            root = root.right
+        return ans
+# Runtime: 32 ms, faster than 92.50% of Python3 online submissions for Binary Tree Inorder Traversal.
+# Memory Usage: 13.8 MB, less than 5.05% of Python3 online submissions for Binary Tree Inorder Traversal.
