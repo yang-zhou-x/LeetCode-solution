bee8857dda328542ba629fc8f3be12ed812a84fa
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-27 21:03:34 +0800

    Create 145. Binary Tree Postorder Traversal.py

---
+++ 101-200/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,45 @@
+'''
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+Output: [3,2,1]
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+'''
+
+# recursion
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        ans = []
+
+        def post(node):
+            if node:
+                post(node.left)
+                post(node.right)
+                ans.append(node.val)
+        post(root)
+        return ans
+# Runtime: 40 ms, faster than 44.93% of Python3 online submissions for Binary Tree Postorder Traversal.
+# Memory Usage: 13.8 MB, less than 5.06% of Python3 online submissions for Binary Tree Postorder Traversal.
+
+# iteration
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        ans = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                # pre-order, but right first
+                ans.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return ans[::-1]  # reverse
+# Runtime: 28 ms, faster than 98.65% of Python3 online submissions for Binary Tree Postorder Traversal.
+# Memory Usage: 13.8 MB, less than 5.06% of Python3 online submissions for Binary Tree Postorder Traversal.
