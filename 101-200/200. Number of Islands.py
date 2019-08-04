57579fbd2b5c7fdfd73fc7502813144c9dab556c
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-04 22:36:35 +0800

    Update 200. Number of Islands.py

--- 101-200/200. Number of Islands.py
+++ 101-200/200. Number of Islands.py
@@ -1,3 +1,26 @@
+'''
+Given a 2d grid map of '1's (land) and '0's (water), 
+count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input:
+11110
+11010
+11000
+00000
+Output: 1
+
+Example 2:
+Input:
+11000
+11000
+00100
+00011
+Output: 3
+'''
+
 class Solution:
     def numIslands(self, grid: 'List[List[str]]') -> 'int':
         def sink(i, j):
@@ -7,5 +30,4 @@ class Solution:
                 return 1
             return 0
         return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
-        
 # Runtime: 96 ms, faster than 34.90% of Python3 online submissions for Number of Islands.
