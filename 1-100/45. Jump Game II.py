9f77bb1f6f89c5957cf23c96df86d5f70f4d9eb3
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-09 23:06:03 +0800

    Update 45. Jump Game II.py

--- 1-100/45. Jump Game II.py
+++ 1-100/45. Jump Game II.py
@@ -13,3 +13,18 @@ Note:
 You can assume that you can always reach the last index.
 '''
 
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        right, step = len(nums) - 1, 0
+        start, end = 0, 0
+        while end < right:
+            step += 1
+            maxend = end + 1
+            for i in range(start, end + 1):
+                if i + nums[i] >= right:  # 能跳到终点
+                    return step
+                maxend = max(maxend, i + nums[i])  # 能跳到的最远位置
+            start, end = end + 1, maxend
+        return step
+# Runtime: 104 ms, faster than 91.55% of Python3 online submissions for Jump Game II.
+# Memory Usage: 16.1 MB, less than 8.33% of Python3 online submissions for Jump Game II.
