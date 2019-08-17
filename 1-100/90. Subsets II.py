12df022ae1c9dcb9c0e6f1c1070a790e8e951164
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-18 00:13:36 +0800

    Update 90. Subsets II.py

--- 1-100/90. Subsets II.py
+++ 1-100/90. Subsets II.py
@@ -15,3 +15,21 @@ Output:
   []
 ]
 '''
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return [[]]
+        nums.sort()
+        ans = [[], [nums[0]]]
+        last = [[nums[0]]]  # 记录上一步加入ans的集合
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                last = [x + [nums[i]] for x in last]
+                ans += last
+            else:
+                last = [x + [nums[i]] for x in ans]
+                ans += last
+        return ans
+# Runtime: 40 ms, faster than 91.29% of Python3 online submissions for Subsets II.
+# Memory Usage: 13.9 MB, less than 6.38% of Python3 online submissions for Subsets II.
