e8068fe69d1c47bdbeebe1b09b332410ece20814
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-20 22:24:20 +0800

    Update 25. Reverse Nodes in k-Group.py

--- 1-100/25. Reverse Nodes in k-Group.py
+++ 1-100/25. Reverse Nodes in k-Group.py
@@ -13,3 +13,28 @@ Only constant extra memory is allowed.
 You may not alter the values in the list's nodes, only nodes itself may be changed.
 '''
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        dummy = jump = ListNode(0)
+        dummy.next = l = r = head
+
+        while True:
+            count = 0
+            while r and count < k:   # use r to locate the range
+                r = r.next
+                count += 1
+            if count == k:  # if size k satisfied, reverse the inner linked list
+                cur, pre = l, r
+                for _ in range(k):
+                    cur.next, cur, pre = pre, cur.next, cur  # standard reversing
+                jump.next, jump, l = pre, l, r  # connect two k-groups
+            else:
+                return dummy.next
+# Runtime: 60 ms, faster than 27.65% of Python3 online submissions for Reverse Nodes in k-Group.
+# Memory Usage: 14.5 MB, less than 5.88% of Python3 online submissions for Reverse Nodes in k-Group.
