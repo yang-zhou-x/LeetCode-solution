42886231a50bdc62fe423e45f8df2f5d76667920
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-21 22:32:37 +0800

    Update 169. Majority Element.py

--- 101-200/169. Majority Element.py
+++ 101-200/169. Majority Element.py
@@ -22,3 +22,19 @@ class Solution:
         return ans
 # Runtime: 44 ms, faster than 97.06% of Python3 online submissions for Majority Element.
 # Memory Usage: 14.3 MB, less than 69.85% of Python3 online submissions for Majority Element.
+
+# easy understanding
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        for n in nums:
+            if count == 0:
+                num = n
+                count = 1
+            elif n == num:
+                count += 1
+            else:
+                count -= 1
+        return num
+# Runtime: 188 ms, faster than 91.81% of Python3 online submissions for Majority Element.
+# Memory Usage: 15.1 MB, less than 7.14% of Python3 online submissions for Majority Element.
