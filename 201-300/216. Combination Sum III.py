73d9b815cadbaa05e731128e7e63c8d85ed6451f
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-24 16:51:58 +0800

    Update 216. Combination Sum III.py

--- 201-300/216. Combination Sum III.py
+++ 201-300/216. Combination Sum III.py
@@ -32,3 +32,25 @@ class Solution:
         return ans
 # Runtime: 40 ms, faster than 52.67% of Python3 online submissions for Combination Sum III.
 # Memory Usage: 13.8 MB, less than 11.11% of Python3 online submissions for Combination Sum III.
+
+
+# 简化一下
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        if n > sum(x for x in range(10-k, 10)):
+            return []
+        ans = []
+
+        def dfs(remain, path, start, t):
+            if remain < 0:
+                return
+            if t == k:
+                if remain == 0:
+                    ans.append(path)
+                return
+            for i in range(start, 11 + t - k):
+                dfs(remain - i, path + [i], i + 1, t + 1)
+        dfs(n, [], 1, 0)
+        return ans
+# Runtime: 32 ms, faster than 95.05% of Python3 online submissions for Combination Sum III.
+# Memory Usage: 13.7 MB, less than 11.11% of Python3 online submissions for Combination Sum III.
