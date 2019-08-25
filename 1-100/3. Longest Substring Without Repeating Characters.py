922be91f182c9a1ee738d8344b6e4fd714e7ca98
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-25 18:48:37 +0800

    Update 3. Longest Substring Without Repeating Characters.py

--- 1-100/3. Longest Substring Without Repeating Characters.py
+++ 1-100/3. Longest Substring Without Repeating Characters.py
@@ -18,26 +18,6 @@ Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 '''
 
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        start=0 # Substring的起始位置
-        ans=0
-        cache={}  # 用来记录每个字母最近一次出现的位置
-        for i,ss in enumerate(s):
-            if ss in cache and start<=cache[ss]:
-                start=cache[ss]+1
-                cache[ss]=i # 更新位置
-            else:
-                ans=max(ans,i-start+1)
-                cache[ss]=i # 添加位置
-        return ans
-# Runtime: 44 ms, faster than 99.14% of Python online submissions for Longest Substring Without Repeating Characters.
-# Memory Usage: 11.2 MB, less than 49.60% of Python online submissions for Longest Substring Without Repeating Characters.
-
 # 2019/07/22
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
@@ -47,9 +27,11 @@ class Solution:
         start = 0
         ans = 0
         for i, ss in enumerate(s):
+            # pos中是所有字母的位置。字串中可能不包含所有字母。
+            # start <= pos[ss] 确保该重复字母出现在当前字串中
             if ss in pos and start <= pos[ss]:
                 ans = max(ans, i - start)
-                start = pos[ss] + 1  # 用来记录每个字母最近一次出现的位置
+                start = pos[ss] + 1  # 更新起始位置
                 pos[ss] = i  # 更新ss的位置
             else:
                 pos[ss] = i  # 添加位置
