579c7781934941009e5909c1106efa2f7b57aee1
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-08-29 19:25:09 +0800

    Update 171. Excel Sheet Column Number.py

--- 101-200/171. Excel Sheet Column Number.py
+++ 101-200/171. Excel Sheet Column Number.py
@@ -1,3 +1,30 @@
+'''
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+
+Example 1:
+Input: "A"
+Output: 1
+
+Example 2:
+Input: "AB"
+Output: 28
+
+Example 3:
+Input: "ZY"
+Output: 701
+'''
+
+# 类似于26进制数
 class Solution(object):
     def titleToNumber(self, s):
         """
@@ -9,4 +36,11 @@ class Solution(object):
             ans=ans*26+ord(ss)-64  # -64=-ord('A')+1
         return ans
 
-# 类似于26进制数
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        ans = 0
+        for cha in s:
+            ans = ans * 26 + ord(cha) - 64  # ord('A') - 64 == 1
+        return ans
+# Runtime: 28 ms, faster than 99.06% of Python3 online submissions for Excel Sheet Column Number.
+# Memory Usage: 14 MB, less than 9.09% of Python3 online submissions for Excel Sheet Column Number.
