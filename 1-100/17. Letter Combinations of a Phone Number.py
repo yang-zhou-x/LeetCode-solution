efa194b58735cb271eaa08ac01d08433a0472007
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-09-01 22:14:58 +0800

    Update 17. Letter Combinations of a Phone Number.py

--- 1-100/17. Letter Combinations of a Phone Number.py
+++ 1-100/17. Letter Combinations of a Phone Number.py
@@ -13,29 +13,6 @@ Although the above answer is in lexicographical order, your answer could be in a
 '''
 
 class Solution:
-    def letterCombinations(self, digits: 'str') -> 'List[str]':
-        phone = {'2': ['a', 'b', 'c'],
-                 '3': ['d', 'e', 'f'],
-                 '4': ['g', 'h', 'i'],
-                 '5': ['j', 'k', 'l'],
-                 '6': ['m', 'n', 'o'],
-                 '7': ['p', 'q', 'r', 's'],
-                 '8': ['t', 'u', 'v'],
-                 '9': ['w', 'x', 'y', 'z']}
-        ans=[]
-        def backTrack(comb,digit):
-            if len(digit)==0:
-                ans.append(comb)
-            else:
-                for letter in phone[digit[0]]:
-                    backTrack(comb+letter,digit[1:])
-        if digits:
-            backTrack('',digits)
-        return ans
-# Runtime: 36 ms, faster than 71.05% of Python3 online submissions for Letter Combinations of a Phone Number.
-# Memory Usage: 13.1 MB, less than 5.86% of Python3 online submissions for Letter Combinations of a Phone Number.
-
-class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         phone = {'2': ['a', 'b', 'c'],
                  '3': ['d', 'e', 'f'],
@@ -46,14 +23,15 @@ class Solution:
                  '8': ['t', 'u', 'v'],
                  '9': ['w', 'x', 'y', 'z']}
         ans = []
-        def helper(path, idx):
+        
+        def back_track(idx, path):
             if idx == len(digits):
                 ans.append(path)
             else:
-                for p in phone[digits[idx]]:
-                    helper(path + p, idx + 1)
+                for cha in phone[digits[idx]]:
+                    back_track(idx + 1, path + cha)
         if digits:
-            helper('', 0)
+            back_track(0, '')
         return ans
 # Runtime: 32 ms, faster than 90.34% of Python3 online submissions for Letter Combinations of a Phone Number.
 # Memory Usage: 14.1 MB, less than 5.23% of Python3 online submissions for Letter Combinations of a Phone Number.
