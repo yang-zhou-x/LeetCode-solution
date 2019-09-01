a2356eb6e862b81b68a6effd13f7bb85d233f332
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-09-01 23:32:59 +0800

    Update 39. Combination Sum.py

--- 1-100/39. Combination Sum.py
+++ 1-100/39. Combination Sum.py
@@ -27,38 +27,18 @@ A solution set is:
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
+        ans = []
         candidates.sort()
-        def dfs(remain, path):
+        
+        def back_track(remain, path, idx):
             if remain == 0:
-                res.append(path)
-                return 
-            for item in candidates:
-                if item > remain:  # early stop
-                    break
-                if path and item < path[-1]:  # keep unique
-                    continue
-                dfs(remain - item, path + [item])
-        dfs(target, [])
-        return res
-# Runtime: 48 ms, faster than 99.89% of Python3 online submissions for Combination Sum.
-# Memory Usage: 13.9 MB, less than 5.05% of Python3 online submissions for Combination Sum.
-
-# 加入索引的版本
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int):
-        res = []
-        candidates.sort()
-
-        def dfs(remain, path, idx):
-            if remain == 0:
-                res.append(path)
-                return
-            for i in range(idx, len(candidates)):
-                if candidates[i] > remain:
-                    break
-                dfs(remain - candidates[i], path + [candidates[i]], i)
-        dfs(target, [], 0)
-        return res
+                ans.append(path)
+            else:
+                for i in range(idx, len(candidates)):
+                    if candidates[i] > remain:
+                        break  # early stop, 后面的数字都大于remain
+                    back_track(remain - candidates[i], path + [candidates[i]], i)
+        back_track(target, [], 0)
+        return ans
 # Runtime: 56 ms, faster than 96.94% of Python3 online submissions for Combination Sum.
 # Memory Usage: 14.1 MB, less than 5.05% of Python3 online submissions for Combination Sum.
