fd2c3065fbc93ae36d0199a76d69f23e751003f0
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-09-01 22:48:49 +0800

    Update 22. Generate Parentheses.py

--- 1-100/22. Generate Parentheses.py
+++ 1-100/22. Generate Parentheses.py
@@ -12,38 +12,18 @@ For example, given n = 3, a solution set is:
 '''
 
 class Solution:
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        ans=[]
-        def f(s='',l=0,r=0):
-            if len(s)==n*2:
-                ans.append(s)
-                return
-            if l<n:
-                f(s+'(',l+1,r)
-            if r<l:
-                f(s+')',l,r+1)
-        f()
-        return ans 
-# Runtime: 36 ms, faster than 99.65% of Python3 online submissions for Generate Parentheses.
-
-# 20190723
-class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-
-        def helper(path, left, right):
+        
+        def back_track(left, right, path):
             if len(path) == n * 2:
                 ans.append(path)
-                return
-            if left < n:
-                helper(path + '(', left + 1, right)
-            if right < left:
-                helper(path + ')', left, right + 1)
-        helper('', 0, 0)
+            else:
+                if left < n:
+                    back_track(left + 1, right, path + '(')
+                if right < left:
+                    back_track(left, right + 1, path + ')')
+        back_track(0, 0, '')
         return ans
 # Runtime: 36 ms, faster than 93.63% of Python3 online submissions for Generate Parentheses.
 # Memory Usage: 14.2 MB, less than 5.01% of Python3 online submissions for Generate Parentheses.
