8b2a6f13940fa8f8d45a8aaea20e2ef42a35bedd
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-09-04 23:18:51 +0800

    Update 131. Palindrome Partitioning.py

--- 101-200/131. Palindrome Partitioning.py
+++ 101-200/131. Palindrome Partitioning.py
@@ -1,18 +1,31 @@
+'''
+Given a string s, partition s such that every substring of the partition is a palindrome.
+Return all possible palindrome partitioning of s.
+
+Example:
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+'''
+
 class Solution:
-    def partition(self, s: 'str') -> 'List[List[str]]':
-        ans=[]
-        self.dfs(s,[],ans)
-        return ans
-    
-    def dfs(self,remain,path,ans):
-        if not remain:
-            ans.append(path) # path储存了所有回文片段
-            return # 终止
-        for i in range(1,len(remain)+1): # 切片，遍历
-            if self.is_palindrome(remain[:i]):
-                self.dfs(remain[i:],path+[remain[:i]],ans) # path记录当前的回文片段
-    
-    def is_palindrome(self,string): # 判断是否回文
-        return string==string[::-1]
+    def partition(self, s: str) -> List[List[str]]:
+        ans = []
         
-# Runtime: 96 ms, faster than 78.10% of Python3 online submissions for Palindrome Partitioning.
+        def is_palindrome(s):
+            return s == s[::-1]
+        
+        def back_track(remain, path):
+            if not remain:
+                ans.append(path)  # path储存了所有回文片段
+            else:
+                for i in range(1, len(remain) + 1):
+                    if is_palindrome(remain[:i]):
+                        back_track(remain[i:], path + [remain[:i]])
+        back_track(s, [])
+        return ans
+# Runtime: 88 ms, faster than 76.44% of Python3 online submissions for Palindrome Partitioning.
+# Memory Usage: 14 MB, less than 5.88% of Python3 online submissions for Palindrome Partitioning.
