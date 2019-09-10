9bc4c09cd824013d31ec4942c3948efa6297dad4
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-09-10 13:31:52 +0800

    Update 414. Third Maximum Number.py

--- 401-500/414. Third Maximum Number.py
+++ 401-500/414. Third Maximum Number.py
@@ -19,3 +19,23 @@ Output: 1
 Explanation: Note that the third maximum here means the third maximum distinct number.
 Both numbers with value 2 are both considered as second maximum.
 '''
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        fir = sec = thi = float('-inf')
+        visited = set()
+        for n in nums:
+            if n in visited:
+                continue
+            elif n > fir:
+                fir, sec, thi = n, fir, sec
+            elif n > sec:
+                sec, thi = n, sec
+            elif n > thi:
+                thi = n
+            else:
+                continue
+            visited.add(n)
+        return thi if thi != float('-inf') else fir
+# Runtime: 60 ms, faster than 85.21% of Python3 online submissions for Third Maximum Number.
+# Memory Usage: 14.6 MB, less than 7.69% of Python3 online submissions for Third Maximum Number.
