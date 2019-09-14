28506ca57dabd069218572f08cc0cd32032dfcc3
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-09-14 22:48:57 +0800

    Update 214. Shortest Palindrome.py

--- 201-300/214. Shortest Palindrome.py
+++ 201-300/214. Shortest Palindrome.py
@@ -10,3 +10,39 @@ Input: "abcd"
 Output: "dcbabcd"
 '''
 
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+        
+        def expand(left, right):
+            if left == right:
+                if s[:left+1] == s[left:left+left+1][::-1]:
+                    return left
+                else:
+                    return float('-inf')
+            else:
+                if s[:left+1] == s[right:right+right][::-1]:
+                    return right
+                else:
+                    return float('-inf')
+
+        center, interval = float('-inf'), False
+        for i in range((len(s) + 1) // 2 - 1, -1, -1):  # 数字为中心
+            tmp = expand(i, i)
+            if tmp > center:
+                center = tmp
+                break
+        for i in range((len(s) + 1) // 2, 0, -1):  # 间隔为中心
+            tmp = expand(i-1, i)
+            if tmp > center:
+                interval = True
+                center = tmp
+                break
+        
+        if interval:
+            return s[center * 2:][::-1] + s
+        else:
+            return s[center * 2 + 1:][::-1] + s
+# Runtime: 300 ms, faster than 30.49% of Python3 online submissions for Shortest Palindrome.
+# Memory Usage: 14.1 MB, less than 14.29% of Python3 online submissions for Shortest Palindrome.
