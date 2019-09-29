4547972f7542ffce1049321a9f04248a83371589
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-09-29 14:28:59 +0800

    Update 560. Subarray Sum Equals K.py

--- 501-600/560. Subarray Sum Equals K.py
+++ 501-600/560. Subarray Sum Equals K.py
@@ -11,3 +11,20 @@ The length of the array is in range [1, 20,000].
 The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
 '''
 
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return int(nums[0] == k)
+        ans = 0
+        cache = {0:1}
+        cumulation = 0
+        for n in nums:
+            cumulation += n
+            if cumulation - k in cache:
+                ans += cache[cumulation-k]
+            cache[cumulation] = cache.get(cumulation, 0) + 1
+        return ans
+# Runtime: 128 ms, faster than 75.27% of Python3 online submissions for Subarray Sum Equals K.
+# Memory Usage: 16 MB, less than 20.00% of Python3 online submissions for Subarray Sum Equals K.
