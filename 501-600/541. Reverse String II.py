dc6bd413d038fde3da5b4fb927a32fbb0628a2e3
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-10-27 00:16:14 +0800

    Update 541. Reverse String II.py

--- 501-600/541. Reverse String II.py
+++ 501-600/541. Reverse String II.py
@@ -12,3 +12,14 @@ Restrictions:
 The string consists of lower English letters only.
 Length of the given string and k will in the range [1, 10000]
 '''
+
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        ans = ''
+        for i in range(0, len(s), 2 * k):
+            tmp = s[i:i+k]
+            ans += tmp[::-1]
+            ans += s[i+k:i+2*k]
+        return ans
+# Runtime: 40 ms, faster than 62.61% of Python3 online submissions for Reverse String II.
+# Memory Usage: 13.9 MB, less than 14.29% of Python3 online submissions for Reverse String II.
