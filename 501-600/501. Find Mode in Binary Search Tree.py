397798e05bc570b5a250e083b1619cbd73be262d
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-11-01 16:48:48 +0800

    Update 501. Find Mode in Binary Search Tree.py

--- 501-600/501. Find Mode in Binary Search Tree.py
+++ 501-600/501. Find Mode in Binary Search Tree.py
@@ -1,5 +1,7 @@
 '''
-Given a binary search tree (BST) with duplicates, find all the mode(s) (the most frequently occurred element) in the given BST.
+Given a binary search tree (BST) with duplicates, find all the mode(s) (the most frequently occurred element) 
+in the given BST.
+
 Assume a BST is defined as follows:
 The left subtree of a node contains only nodes with keys less than or equal to the node's key.
 The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
@@ -19,3 +21,35 @@ Follow up: Could you do that without using any extra space? (Assume that the
 implicit stack space incurred due to recursion does not count).
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import Counter
+
+class Solution:
+    def findMode(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        nums = []
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            nums.append(root.val)
+            root = root.right
+        nums = Counter(nums)
+        # maxi_key = sorted(nums, key=d.get, reverse=True)[0]
+        maxi_cnt = sorted(nums.values())[-1]
+        ans = []
+        for key in nums:
+            if nums[key] == maxi_cnt:
+                ans.append(key)
+        return ans
+# Runtime: 64 ms, faster than 78.46% of Python3 online submissions for Find Mode in Binary Search Tree.
+# Memory Usage: 18 MB, less than 8.33% of Python3 online submissions for Find Mode in Binary Search Tree.
