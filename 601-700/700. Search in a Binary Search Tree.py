48a425902bcd2a6e0c9da8c1816b922e39b099ba
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-12-25 13:29:17 +0800

    Create 700. Search in a Binary Search Tree.py

---
+++ 601-700/700. Search in a Binary Search Tree.py
@@ -0,0 +1,43 @@
+'''
+Given the root node of a binary search tree (BST) and a value. 
+You need to find the node in the BST that the node's value equals the given value. 
+Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example, 
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+And the value to search: 2
+You should return this subtree:
+      2     
+     / \   
+    1   3
+In the example above, if we want to search the value 5, 
+since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, 
+therefore you would see the expected output (serialized tree format) as [], not null.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+# Runtime: 76 ms, faster than 77.98% of Python3 online submissions for Search in a Binary Search Tree.
+# Memory Usage: 14.7 MB, less than 100.00% of Python3 online submissions for Search in a Binary Search Tree.
